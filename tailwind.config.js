bc253b2977de7b93f47e9b790851aa689a5fdace
jesus <jesus_lopez1997@hotmail.com>
2021-05-16 01:36:12 -0400

    Add custom gray palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      gray: colors.trueGray,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
 };
+// colors.warmGray
